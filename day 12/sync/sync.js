a3ae7abadaeac34099784d20ae703d3cda93c2b4
abshek7 <abhishekallaboina@gmail.com>
2026-01-13 14:54:13 +0530

    sync related js works

---
+++ day 12/sync/sync.js
@@ -0,0 +1,12 @@
+// this runs synchronously
+//goes line by line
+//at each place browser waits the line to finish 
+const nickname="Abhi";
+const logging=`This is variable name ${nickname}`;
+console.log(logging)
+// console.log(displayLogs(nickname))
+
+// function displayLogs(nickname) {
+//   return `Hello, my name is ${nickname}!`;
+// }
+
