57ab61bd0aad455cf5bdec3d745389abcf6b8b38
abshek7 <abhishekallaboina@gmail.com>
2026-02-02 15:17:43 +0530

    .net overflow check vs uncheck

--- dotNetWeeklyAssignments/DAY-3/overflow/Program.cs
+++ dotNetWeeklyAssignments/DAY-3/overflow/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp1
+﻿namespace overflow
 {
     public class Program
     {
--- dotNetWeeklyAssignments/DAY-3/overflow/checked.cs
+++ dotNetWeeklyAssignments/DAY-3/overflow/checked.cs
@@ -1,5 +1,5 @@
-﻿ 
-namespace ConsoleApp1
+﻿
+namespace overflow
 {
     public class Checked
     { public static void Run()
