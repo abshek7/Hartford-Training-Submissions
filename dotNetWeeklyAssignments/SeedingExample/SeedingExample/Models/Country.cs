4ab966e66675fbe4ad78224839e323a955cef0b1
abshek7 <abhishekallaboina@gmail.com>
2026-02-17 17:21:04 +0530

    seeding data example

---
+++ dotNetWeeklyAssignments/SeedingExample/SeedingExample/Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace SeedingExample.Models
+
+{
+    [Table("Countries")]
+    public class Country
+    {
+        [Key]
+        public int CountryId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string CountryName { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string CountryCode { get; set; }
+
+        // Navigation Property
+        public ICollection<State> States { get; set; }
+    }
+}
