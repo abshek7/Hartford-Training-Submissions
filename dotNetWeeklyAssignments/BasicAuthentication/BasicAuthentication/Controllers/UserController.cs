6e980b9e3778690a1fafa76eb3b401907b5bd760
abshek7 <abhishekallaboina@gmail.com>
2026-02-17 17:22:01 +0530

    basic hardcode auth

---
+++ dotNetWeeklyAssignments/BasicAuthentication/BasicAuthentication/Controllers/UserController.cs
@@ -0,0 +1,79 @@
+﻿using BasicAuthentication.DTOs;
+using BasicAuthentication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext _userContext;
+        // construction injection
+        public UserController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+         
+
+        // method for user registration
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult Register(UserDto user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var curr = _userContext.Users
+                .FirstOrDefault(u => u.Email == user.Email);
+
+            if (curr != null)
+            {
+                return BadRequest("User already exists");
+            }
+
+            var currUser = new User
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Password = user.Password,
+                CreatedOn = DateTime.Now,
+                isActive = true
+            };
+
+            _userContext.Users.Add(currUser);
+            _userContext.SaveChanges();
+
+            return Ok("User reg successful");
+        }
+
+
+
+        // method for user login 
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(LoginDto user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var validUser = _userContext.Users.FirstOrDefault(u =>
+                u.Email == user.Email &&
+                u.Password == user.Password &&
+                u.isActive == true);
+
+            if (validUser == null)
+            {
+                return Unauthorized("Invalid Email or Password");
+            }
+
+            return Ok(validUser);
+        }
+    }
+}
---
+++ dotNetWeeklyAssignments/BasicAuthentication/BasicAuthentication/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicAuthentication.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool isActive { get; set; } = true;
+
+        public DateTime CreatedOn { get; set; }=DateTime.Now;
+
+    }
+}
